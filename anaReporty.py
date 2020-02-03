8ebe60c3a17e046f73b62606de11d32101c73839
fantianwen <fantianwen121@gmail.com>
2020-02-03 23:45:36 +0900

    good ana

--- anaReporty.py
+++ anaReporty.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 import os
 
-anaFileRoot = '/home/radasm/GoProjects/katago_Tools/ana_report'
+anaFileRoot = '/home/radasm/GoProjects/katago_Tools/ana_report_good'
 # anaTestFile = '/home/radasm/GoProjects/katago_Tools/ana_report/Kat01_cho_0323_1.report'
 anaPD = pd.DataFrame(columns=['name', 'move', 'wr', 'tr'])
 # anaPD.append(pd.Series(['222', '22', '11', '22'], index=anaPD.columns), ignore_index=True)
@@ -21,11 +21,12 @@ def setInfo(name, anaText):
 
         anaPD.loc[len(anaPD)] = [name, turnMove, winrate, scoreLead]
 
-def setDiffInfo(name,anaText):
+
+def setDiffInfo(name, anaText):
     for moveAnaReport in anaText:
         turnMove = moveAnaReport['turnNumber']
 
-        if turnMove%2 == 1:
+        if turnMove % 2 == 1:
             moveInfos = moveAnaReport['moveInfos']
             bestMoveInfo = moveInfos[0]
             winrate = bestMoveInfo['winrate']
@@ -38,19 +39,21 @@ def setDiffInfo(name,anaText):
                     bestMoveInfo_ = moveInfos_[0]
                     winrate_ = bestMoveInfo_['winrate']
                     scoreLead_ = bestMoveInfo_['scoreLead']
-                    anaPD_BA.loc[len(anaPD_BA)] = [name, turnMove, winrate, winrate_, winrate_-winrate, scoreLead,scoreLead_, scoreLead_-scoreLead]
+                    anaPD_BA.loc[len(anaPD_BA)] = [name, turnMove, winrate, winrate_, winrate_ - winrate, scoreLead,
+                                                   scoreLead_, scoreLead_ - scoreLead]
 
                     break
 
+
 for anaFile_ in os.listdir(anaFileRoot):
     if not os.path.isdir(anaFile_):
-        with open(anaFileRoot+'/'+anaFile_, 'rt') as anafile:
+        with open(anaFileRoot + '/' + anaFile_, 'rt') as anafile:
             anaText = json.loads(anafile.read())
             # print(anaText)
             nameWithType = os.path.basename(anaFile_)
             moveFileName = nameWithType[:-7]
             # setInfo(name=moveFileName, anaText=anaText)
-            setDiffInfo(name=moveFileName,anaText = anaText)
+            setDiffInfo(name=moveFileName, anaText=anaText)
 
 # anaPD.to_csv('testAnaExcel.csv')
 anaPD_BA.to_csv('testAnaDiff.csv')
