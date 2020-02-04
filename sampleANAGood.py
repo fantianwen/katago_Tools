8e9f53d94f96b6c5eb1a54d5f840aaeb79770504
fantianwen <fantianwen121@gmail.com>
2020-02-04 16:12:14 +0900

    sampleGood

---
+++ sampleANAGood.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import pandas as pd
+
+anaGood = pd.read_csv('testAnaGoodDiff.csv')
+anaGoodSampled = anaGood.sample(n=2000, replace=False)
+
+anaGoodSampled.to_csv('anaGoodSampled.csv')
