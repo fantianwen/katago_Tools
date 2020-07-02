1624a8edcacd5143a178c01b3f5877f52524ffab
fantianwen <fantianwen121@gmail.com>
2020-07-02 17:13:09 +0900

    for good analysis

--- auto_analysis.py
+++ auto_analysis.py
@@ -183,8 +183,8 @@ enginne = GTPFacade("kata", KataGo)
 
 time.sleep(8)
 
-RootPath = '/home/ikeda-05444/users/fan/GoProjects/katago_Tools/dec_ana'
-RootAnaReportPath = '/home/ikeda-05444/users/fan/GoProjects/katago_Tools/ana_report'
+RootPath = '/home/ikeda-05444/users/fan/GoProjects/katago_Tools/dec_ana_good'
+RootAnaReportPath = '/home/ikeda-05444/users/fan/GoProjects/katago_Tools/ana_report_good'
 
 def saveAnaToFile(anaText, rootName, fileName):
     forsave = rootName + '/' + fileName + '.report'
